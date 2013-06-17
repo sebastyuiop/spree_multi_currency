8ad214aeae530f70351956eeea8078d8e58e36ce
Seb <sebweston@gmail.com>
2013-06-17 23:21:11 +1000

    not sure if this will work

--- lib/spree/core/controller_helpers/common_decorator.rb
+++ lib/spree/core/controller_helpers/common_decorator.rb
@@ -2,7 +2,7 @@ Spree::Core::ControllerHelpers::Common.class_eval do
   def current_currency
     # ensure session currency is supported
     #
-    if session.key?(:currency) && supported_currencies.map(&:iso_code).include?(session[:currency])
+    if session.key?(:currency)# && supported_currencies.map(&:iso_code).include?(session[:currency])
       session[:currency]
     else
       Spree::Config[:currency]
